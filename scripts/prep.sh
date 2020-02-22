dda1592dc9c7d818bee6af07fcd14310b4a821ce
Atom Scott <atom.james.scott@gmail.com>
2020-02-22 23:18:46 +0900

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -4,6 +4,9 @@ atcoder-tools gen $id --workspace './' --lang python < ./scripts/.secret
 code ./$id/A/main.py
 cd ./$id
 
+xdg-open https://nbviewer.jupyter.org/github/AtomScott/atcoder/blob/master/00_notebook_lib.ipynb
+
+
 echo 'Just in case you forget..'
 echo 'Theres no need to change dir, you can do everything from the command line'
 echo 'open new main.py files from the cmd with code A/main.py'
